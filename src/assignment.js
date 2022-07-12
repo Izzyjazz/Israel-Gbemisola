59ddc026a89e20de2551073609e6b71db74e15c4
Gbemisola Israel <106116554+Izzyjazz@users.noreply.github.com>
2022-07-12 21:44:40 +0000

    check challenge 1 check challenge 2

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,9 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    return arrayOfNumbers.reduce((previous, current)=>{ return previous + current},0)
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+ assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +28,11 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+const acc = arrayOfNumbers.filter(arr =>
+  arr % 2 == 0);
+  return acc.length
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+  assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
